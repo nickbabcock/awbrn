ba6dac3ed32e955c96f47bbcf02087c66a80b8af
Nick Babcock <nbabcock19@hotmail.com>
2025-04-15 18:42:31 -0500

    Commit texture generation

---
+++ assets/textures/generate-spritesheet.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DIR="$( dirname -- "${BASH_SOURCE[0]}"; )";
+cd "$DIR"
+
+# Fill in with the path to AWBW-Replay-Player/AWBWApp.Resources/Textures
+TEXTURES_DIR="${TEXTURES_DIR:-.}"
+
+montage -tile 64x -mode concatenate -gravity southeast -geometry '16x32>' -background transparent -define webp:lossless=true $(cat \
+   <(find "$TEXTURES_DIR/Map/AW2" -maxdepth 1 -type f -not -name "*_Snow*" -not -name "*_Rain*" | sort) \
+   <(echo "stubby.png") \
+   <(find "$TEXTURES_DIR/Map/AW2" -mindepth 2 -type f -not -name "*_Snow*" -not -name "*_Rain*" | sort) \
+   <(find "$TEXTURES_DIR/Map/AW2" -maxdepth 1 -type f -name "*_Snow*" | sort) \
+   <(echo "stubby-snow.png") \
+   <(find "$TEXTURES_DIR/Map/AW2" -mindepth 2 -type f -name "*_Snow*" | sort) \
+   <(find "$TEXTURES_DIR/Map/AW2" -maxdepth 1 -type f -name "*_Rain*" | sort) \
+   <(find "$TEXTURES_DIR/Map/AW2" -mindepth 2 -type f -name "*_Rain*" | sort)
+) tiles.webp
+
+montage -tile 64x -mode concatenate -gravity southeast -geometry '23x24>' -background transparent -define webp:lossless=true \
+    $(find "$TEXTURES_DIR/Units" -type f | sort) units.webp
