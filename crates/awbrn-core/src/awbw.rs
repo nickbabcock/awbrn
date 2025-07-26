90fe093887c5c995c266223cf333ef3438f46771
Nick Babcock <nbabcock19@hotmail.com>
2025-07-26 14:42:20 -0500

    Update replay models to support better team fog

--- crates/awbrn-core/src/awbw.rs
+++ crates/awbrn-core/src/awbw.rs
@@ -41,7 +41,7 @@ impl AwbwGameId {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize)]
 #[serde(transparent)]
 pub struct AwbwUnitId(u32);
 
@@ -52,6 +52,60 @@ impl AwbwUnitId {
 
     pub const fn as_u32(&self) -> u32 {
         self.0
+    }
+}
+
+impl<'de> Deserialize<'de> for AwbwUnitId {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: serde::Deserializer<'de>,
+    {
+        struct UnitIdVisitor;
+
+        impl serde::de::Visitor<'_> for UnitIdVisitor {
+            type Value = AwbwUnitId;
+
+            fn expecting(&self, formatter: &mut std::fmt::Formatter) -> std::fmt::Result {
+                formatter.write_str("a number or numeric string")
+            }
+
+            fn visit_u32<E>(self, value: u32) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                Ok(AwbwUnitId::new(value))
+            }
+
+            fn visit_u64<E>(self, value: u64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let val = u32::try_from(value)
+                    .map_err(|_| E::custom(format!("Unit ID out of range: {}", value)))?;
+                Ok(AwbwUnitId::new(val))
+            }
+
+            fn visit_i64<E>(self, value: i64) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let val = u32::try_from(value)
+                    .map_err(|_| E::custom(format!("Unit ID out of range: {}", value)))?;
+                Ok(AwbwUnitId::new(val))
+            }
+
+            fn visit_str<E>(self, value: &str) -> Result<Self::Value, E>
+            where
+                E: serde::de::Error,
+            {
+                let val = value
+                    .parse::<u32>()
+                    .map_err(|_| E::custom(format!("Invalid unit ID: {}", value)))?;
+                Ok(AwbwUnitId::new(val))
+            }
+        }
+
+        deserializer.deserialize_any(UnitIdVisitor)
     }
 }
 
