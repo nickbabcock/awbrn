78aa69133efd2f22af77b1349ad644a445b3737c
Nick Babcock <nbabcock19@hotmail.com>
2026-04-04 07:05:06 -0500

    Cache remote responses to be a good netizen

--- web/src/routes/api/awbw/map.$mapId.ts
+++ web/src/routes/api/awbw/map.$mapId.ts
@@ -1,17 +1,17 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { fetchAwbwMap } from "#/awbw/awbw.server.ts";
+import { fetchAwbwMapResponse } from "#/awbw/awbw.server.ts";
 import { parsePositiveIntegerParam } from "#/awbw/parsers.ts";
 
 export const Route = createFileRoute("/api/awbw/map/$mapId")({
   server: {
     handlers: {
-      GET: async ({ params }) => {
+      GET: async ({ params, request }) => {
         const mapId = parsePositiveIntegerParam(params.mapId.replace(/\.json$/i, ""));
         if (mapId === null) {
           return Response.json({ error: "Invalid mapId" }, { status: 400 });
         }
 
-        return fetchAwbwMap(mapId);
+        return fetchAwbwMapResponse(request, mapId);
       },
     },
   },
